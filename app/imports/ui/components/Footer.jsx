c40af4fc3930cfc14ca592af545044a12d545ea7
Kevin Liu <liukevin@hawaii.edu>
2019-04-24 23:48:53 -1000

    updated footer functionality & coloring

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Grid, Input, Icon } from 'semantic-ui-react';
-import PropTypes from 'prop-types';
 import { Bert } from 'meteor/themeteorchef:bert';
+import { Menu } from 'semantic-ui-react/dist/commonjs/collections/Menu';
 
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 class Footer extends React.Component {
@@ -15,29 +15,24 @@ class Footer extends React.Component {
   }
 
   render() {
-    const divStyle = { paddingTop: '5px', paddingBottom: '20px', color: 'white' };
-    const background = { background: '#292929' };
+    const divStyle = { paddingTop: '15px', background: '#009443', color: 'white' };
     return (
-        <footer style={background}>
-          <div style={divStyle} className="ui center aligned container">
-            <hr/>
-            <div style={background}>
-              <Grid>
-                <Grid.Row centered>
-                  BOUJEE FOODIE
-                </Grid.Row>
-                <Grid.Row centered>
-                  <Icon size='large' name='facebook'/>
-                  <Icon size='large' name='twitter'/>
-                  <Icon size='large' name='instagram'/>
-                </Grid.Row>
-                <Grid.Row centered>
-                  <Input action={{ content: 'Join Newsletter', onClick: this.handleClick }} placeholder='Enter Email Address'/>
-                </Grid.Row>
-              </Grid>
-            </div>
-          </div>
-        </footer>
+        <div style={divStyle}>
+          <Grid columns={3} container centered>
+            <Grid.Column>
+              <h1>BOUJEE FOODIE</h1>
+            </Grid.Column>
+            <Grid.Column>
+              <a href='https://www.facebook.com' style={{color: 'white'}}><Icon size='big' name='facebook'/></a>
+              <a href='https://www.twitter.com' style={{color: 'white'}}><Icon size='big' name='twitter' link/></a>
+              <a href='https://www.instagram.com' style={{color: 'white'}}><Icon size='big' name='instagram' link/></a>
+            </Grid.Column>
+            <Grid.Column>
+              <Input action={{ content: 'Join Newsletter', onClick: this.handleClick }}
+                     placeholder='Enter Email Address'/>
+            </Grid.Column>
+          </Grid>
+        </div>
     );
   }
 }
